1d898920f1fe281886900f12df7add2340fe0305
brickstar <brickstar@gmail.com>
2018-07-27 10:48:42 -0600

    second to last story done

---
+++ spec/features/trail_show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'user visits trips show' do
+  before(:each) do
+    @trail = Trail.create!(length: 1, name: 'sketchy', address: 'main st')
+    @trip1 = @trail.trips.create!(name: 'thundertrip', start_date: 'Tue, 04 Aug 2015', end_date: 'Tue, 04 Aug 2015')
+    @trail2 = @trip1.trails.create!(length: 1, name: 'rocky', address: 'main st')
+    @trail3 = @trip1.trails.create!(length: 2, name: 'freaky', address: 'first st')
+    @trip2 = @trail.trips.create!(name: 'raintrip', start_date: 'Tue, 04 Aug 2015', end_date: 'Tue, 04 Aug 2015')
+    @trail4 = @trip2.trails.create!(length: 5, name: 'scary', address: 'second st')
+    @trail5 = @trip2.trails.create!(length: 6, name: 'easy', address: 'third st')
+  end
+
+  it 'should see name, address and total length of every trip that includes trail' do
+    visit trail_path(@trail)
+
+    expect(page).to have_content(@trip1.name)
+    expect(page).to have_content(@trip2.name)
+    expect(page).to have_content(@trail.address)
+    expect(page).to have_content(@trail2.address)
+    expect(page).to have_content(@trail3.address)
+    expect(page).to have_content(@trail4.address)
+    expect(page).to have_content(@trail5.address)
+    expect(page).to have_content("Total trip length: #{@trip1.total_hiking_distance}")
+    expect(page).to have_content("Total trip length: #{@trip2.total_hiking_distance}")
+  end
+
+end
